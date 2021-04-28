b421e5b52430295c34b63211fed4f9235b09c2b1
Chowdhury-Joy <62534869+Chowdhury-Joy@users.noreply.github.com>
2021-04-28 21:04:50 +0600

    update 03

--- js/nav-whole.js
+++ js/nav-whole.js
@@ -1,6 +1,11 @@
-const hamburger = document.getElementById("hamburger");
-const navUL = document.getElementById('nav-ul');
+// const hamburger = document.getElementById("hamburger");
+// const navUL = document.getElementById('nav-ul');
 
-hamburger.addEventListener('click', () => {
-    navUL.classList.toggle('show');
-});
+// hamburger.addEventListener('click', () => {
+//     navUL.classList.toggle('show');
+// });
+
+function toggle() {
+    var header = document.querySelector(".nav-right");
+    header.classList.toggle("active");
+};
