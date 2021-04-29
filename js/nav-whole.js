c18b1c0588aa6e2d152ca1bc78f65d46c8990e89
Chowdhury-Joy <62534869+Chowdhury-Joy@users.noreply.github.com>
2021-04-29 14:22:36 +0600

    front page responsive done

--- js/nav-whole.js
+++ js/nav-whole.js
@@ -1,11 +1,6 @@
-// const hamburger = document.getElementById("hamburger");
-// const navUL = document.getElementById('nav-ul');
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById('nav-ul');
 
-// hamburger.addEventListener('click', () => {
-//     navUL.classList.toggle('show');
-// });
-
-function toggle() {
-    var header = document.querySelector(".nav-right");
-    header.classList.toggle("active");
-};
+hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+});
